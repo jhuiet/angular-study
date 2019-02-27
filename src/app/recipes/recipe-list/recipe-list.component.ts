9848559cb31d0f2a40fb3fe201eda73f218b23ac
jacob <huietj1@nku.edu>
2019-02-27 10:08:41 -0500

    basic page has been setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.sass']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('potata soup', 'Delicious taters', 'https://www.maxpixel.net/static/photo/1x/Nutrition-Potato-Soup-Onion-Cheddar-Cheese-Bacon-655123.jpg'),
+    new Recipe('potata soup', 'Delicious taters', 'https://www.maxpixel.net/static/photo/1x/Nutrition-Potato-Soup-Onion-Cheddar-Cheese-Bacon-655123.jpg')
+  ];
+
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.sass']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('lettuce', 3)
+
+  ];
 
   constructor() { }
 
