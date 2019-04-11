fcefa417d1ad04f00f4862b55b1d8e649a2696c3
jacob <huietj1@nku.edu>
2019-04-11 10:34:38 -0400

    template driven approach to forms added to the shopping-edit component

--- src/app/services/shopping.service.ts
+++ src/app/services/shopping.service.ts
@@ -3,24 +3,43 @@ import { Subject } from 'rxjs';
 
 export class ShoppingService {
   ingredientsChange = new Subject<Ingredient[]>();
+  startedEdit = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient("apples", 5),
     new Ingredient("lettuce", 3)
   ];
 
-  addIngredient(ingredient: Ingredient){
+  addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
     this.ingredientsChange.next(this.ingredients.slice());
   }
 
-  addIngredients(ingredients: Ingredient[]){
+  addIngredients(ingredients: Ingredient[]) {
   //   for(let i of ingredients){
   //     this.ingredients.push(i);
   //   }
     this.ingredients.push(...ingredients);
     this.ingredientsChange.next(this.ingredients.slice());
   }
-  getIngredients(){
+  getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
+  }
+
+  updateIngredient(index: number, ingredient: Ingredient) {
+    this.ingredients[index] = ingredient;
+    this.ingredientsChange.next(this.ingredients.slice());
+    this.ingredientsChange.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(ingredient: Ingredient) {
+    var index = this.ingredients.findIndex(x => x.amount === ingredient.amount && x.name === ingredient.name);
+    if (index > -1) {
+      this.ingredients.splice(index, 1);
+    }
+    this.ingredientsChange.next(this.ingredients.slice());
   }
 }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,28 +1,80 @@
-import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingService } from 'src/app/services/shopping.service';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+import { extendsDirectlyFromObject } from '@angular/core/src/render3/jit/directive';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.sass']
 })
-export class ShoppingEditComponent implements OnInit {
-  @ViewChild('nameInput') name: ElementRef;
-  @ViewChild('amountInput') amount: ElementRef;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
 
 
+  /*
+    This component demonstrates the template driven approach of form handling.
+    It uses the shopping.service.ts service to modify the list of ingredients.
+
+
+
+
+
+
+
+
+  */
+  @ViewChild('f') slForm: NgForm;
+
+  subscription: Subscription;
+  editMode = false;
+  editItemIndex: number;
+  editItem: Ingredient;
   constructor(private shoppingService: ShoppingService) { }
 
   ngOnInit() {
+    this.subscription = this.shoppingService.startedEdit.subscribe(
+      (index: number) => {
+        this.editMode = true;
+        this.editItemIndex = index;
+        this.editItem = this.shoppingService.getIngredient(this.editItemIndex);
+        this.slForm.setValue({
+          name: this.editItem.name,
+          amount: this.editItem.amount
+        });
+      }
+    );
   }
 
-  addItem() {
-    const ingName = this.name.nativeElement.value;
-    const ingAmount = this.amount.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
-    this.shoppingService.addIngredient(newIngredient);
+
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  onSubmit() {
+    const value = this.slForm.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if (this.editMode) {
+      this.shoppingService.updateIngredient(this.editItemIndex, newIngredient);
+      this.editMode = false;
+      this.slForm.reset();
+    } else {
+      this.shoppingService.addIngredient(newIngredient);
+    }
   }
 
 
+  onDelete() {
+    this.shoppingService.deleteIngredient(this.editItem);
+    this.editMode = false;
+    this.slForm.reset();
+  }
+
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
+  }
 }
